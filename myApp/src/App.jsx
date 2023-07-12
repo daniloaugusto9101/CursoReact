6eebcc80d09cf3392cf776d14ea410a98a1f8b33
Danilo Augusto <daniloaugusto9101@gmail.com>
2023-07-12 09:14:12 -0300

    Add router

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import Animation from './components/animation/Animation'
-
+import { } from 'react-router-dom'
 
 const App = () => {
-    const [ativar, setAtivar] = React.useState(false)
 
     return (<>
-        <button
-            onClick={() => setAtivar(!ativar)}
-            className='bg-violet-900 text-white p-4 wiggle'>
-            Ativar
-        </button>
-        {ativar && <Animation />}
-
 
     </>
     );
