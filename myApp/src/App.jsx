893070bdafa8807777f0ff27c5fc6faeea88a5fd
Danilo Augusto <daniloaugusto9101@gmail.com>
2023-07-12 11:01:07 -0300

    bkp arquivos

--- myApp/src/App.jsx
+++ myApp/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Animation from './components/animation/Animation'
+import NoutFound from './components/pages/NoutFound'
+import Header from './components/pages/Header';
+import Sobre from './components/pages/Sobre';
+import Produto from './components/pages/Produto';
 
 const App = () => {
 
-    return (<>
-
-    </>
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='sobre' element={<Sobre />} />
+                <Route path='animation' element={<Animation />} />
+                <Route path='produto/:id' element={<Produto />} />
+                <Route path='*' element={<NoutFound />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
--- myApp/src/components/animation/Animation.jsx
+++ myApp/src/components/animation/Animation.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Animation = () => {
+    const navigate = useNavigate();
+
+    function handleClick() {
+        console.log('Fez autenticaação');
+        navigate('/')
+    }
+
     return (
         <div className='animate-slide-left'>
             <h1 className='text-2xl'>Meu produto</h1>
             <p>Lorem ipsum, dolor sit amet </p>
-
+            <button
+                onClick={handleClick}
+                className='bg-blue-300 p-3'>
+                Login
+            </button>
         </div>
     )
 }
